35e0ca81733bae3a65bed7ee8d6e36a2f0fa0ed4
sunshead <sunshead@gmail.com>
2016-06-10 11:58:35 -0400

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'access#index'
+  root 'questions#index'
 
-  get 'admin', :to => 'access#index'
+  #get 'admin', :to => 'access#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
